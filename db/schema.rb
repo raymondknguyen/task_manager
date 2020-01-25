6ec3ae05593cae99367b82472accf91f36ceef06
Raymond Nguyen <rayknguyen@gmail.com>
2020-01-25 13:18:13 -0700

    New Task Manager

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_01_25_200458) do
+
+  create_table "tasks", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+  end
 
 end
